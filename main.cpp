7a9a7bd1b98819e0132cfecefc9153a03143cfa7
dragonmost <dragonmost@live.ca>
2015-05-31 02:11:42 -0400

    Learning Sprite
    Learning to use events, textures and sprites

--- main.cpp
+++ main.cpp
@@ -1,32 +1,70 @@
 #include <Windows.h>
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 using namespace sf;
+using namespace std;
 
 int main()
 {
-	HWND hWnd = GetConsoleWindow();
-	ShowWindow(hWnd, SW_HIDE);
+	//HWND hWnd = GetConsoleWindow();
+	//ShowWindow(hWnd, SW_HIDE);
 
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Bleb n' Babas Tactical");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Bleb n' Babas Tactical", sf::Style::Close);
+	//window.setVerticalSyncEnabled(true);
+	/* Never use both setVerticalSyncEnabled and setFramerateLimit at the same time! 
+	 * They would badly mix and make things worse.*/
+	window.setFramerateLimit(120);
+
+	Texture texture;
+	if (!texture.loadFromFile("image.jpg"))
+	{
+		cout << "Texture pas loader" << endl;
+	}
+
+	Sprite sprite;
+	sprite.setTexture(texture);
+	
 
 	while (window.isOpen())
 	{
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			switch (event.type)
+			{
+			case Event::Closed:
 				window.close();
+				break;
+
+			case Event::KeyPressed:
+				break;
+
+			case Event::JoystickButtonPressed:
+				break;
+			case Event::JoystickMoved:
+				break;
+
+			default:
+				break;
+			}
+			
+			window.clear(Color::Black);
+			window.draw(sprite);
+			window.display();
 		}
-
-	
-
-		window.clear();
-		window.draw(shape);
-		window.display();
 	}
 
 	return 0;
 }
+
+
+
+
+void Game()
+{
+	
+
+}
+
+
